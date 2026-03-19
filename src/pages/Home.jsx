2dae562fc0f0029f16388c41acfa9a85411084c0
samridhi8595-lab <samridhi8595@gmail.com>
2026-03-19 17:03:19 +0530

    complete testimonial section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import FeaturedDestination from '../components/FeaturedDestination'
+import ExclusiveOffers from '../components/ExclusiveOffers'
+import Testimonial from '../components/Testimonial'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <FeaturedDestination/> 
+      <ExclusiveOffers/>
+      <Testimonial/>
     </div>
   )
 }
