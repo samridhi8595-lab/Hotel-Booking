ad8ba46d702ff5c4154958e3719274d3b58241a8
samridhi8595-lab <samridhi8595@gmail.com>
2026-03-29 15:27:22 +0530

    compeleted RoomList Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,31 @@ import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
 import RoomDetails from './pages/RoomDetails';
 import MyBookings from './pages/MyBookings';
+import HotelReg from './components/HotelReg';
+import Layout from './pages/HotelOwner/Layout';
+import Dashboard from './pages/HotelOwner/Dashboard';
+import AddRoom from './pages/HotelOwner/AddRoom';
+import RoomList from './pages/HotelOwner/RoomList';
 
 const App = () => {
     const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
       {!isOwnerPath && <Navbar/>}
+      {false && <HotelReg/>}
       <div className='min-h-[70vh]'>
         <Routes>
           <Route path='/' element={<Home/>}/> 
           <Route path='/rooms' element={<AllRooms/>}/> 
           <Route path='/rooms/:id' element={<RoomDetails/>}/> 
            <Route path='/my-bookings' element={<MyBookings/>}/> 
+           <Route path='/owner' element={<Layout/>}>
+              <Route index element={<Dashboard/>} />
+              <Route path="add-room" element={<AddRoom/>} />
+              <Route path="list-room" element={<RoomList/>} />
+              
+              
+            </Route>
         </Routes>
       </div>
       <Footer/>
